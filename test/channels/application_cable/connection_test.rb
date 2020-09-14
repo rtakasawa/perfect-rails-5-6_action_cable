96f6d8f1c642060530295073dcfa0704d2eaa0e5
ryosuke takasawa <ten.r0619@gmail.com>
2020-09-15 05:22:50 +0900

    276p

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "42"
-  # end
+  test "connects accepts" do
+    user = User.first
+    cookies.singed[:user_id] = user.id
+    connect
+    assert_equal connection.current.user_id, user.id
+  end
+
+  test "connection rejects" do
+    cookies.singed[:user_id] = nil
+    assert_reject_connection { connect }
+  end
 end
